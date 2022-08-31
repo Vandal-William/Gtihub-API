ff64e39d6c838e1c8c83f6fb9b478223c4c8a824
Vandal-William <vandal.william@hotmail.com>
2022-08-31 08:32:41 +0200

    css page 404

--- src/components/PageError/index.js
+++ src/components/PageError/index.js
@@ -1,10 +1,14 @@
 // == Composant
 
 import { Segment } from 'semantic-ui-react';
+import './style.scss';
 
 function PageError() {
   return (
-    <Segment> 404 Page Not Found </Segment>
+    <Segment className="error">
+      <span className="error-number">404</span>
+      <span className="error-message">Page Not Found</span>
+    </Segment>
   );
 }
 
